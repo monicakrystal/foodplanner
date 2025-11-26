774b53a41f94551d892f7526d05b4802b0f82233
monica <monicakrystal@icloud.com>
2025-11-26 11:13:25 -0800

    add buttons and style

--- app/components/NavBar/NavBar.tsx
+++ app/components/NavBar/NavBar.tsx
@@ -3,7 +3,14 @@ import styles from "./NavBar.module.css"
 const NavBar = () => {
   return (
     <div className={styles.navbar}>
+      <div>
+
         <h1>Food Bestie</h1>
+      </div>
+      <div className={styles.buttons}>
+<button> Recipes </button>
+<button> Calendar </button>        
+      </div>
     </div>
   )
 }
