30069208be072fba137955e89b2385559d4a9836
monica <monicakrystal@icloud.com>
2025-11-26 19:38:23 -0800

    added recipe title; slight flex design

--- app/recipes/page.tsx
+++ app/recipes/page.tsx
@@ -6,7 +6,13 @@ import styles from "./pages.module.css"
 const Page = () => {
   return (
     <div className={styles.container}>
+      <div className={styles.topText}>
+        <h1>Recipes</h1>
+      </div>
+      <div className={styles.recipesContainer}>
+        
       <RecipeThumbnail/>
+      </div>
     </div>
   )
 }
