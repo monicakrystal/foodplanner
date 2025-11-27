9ca3dcb2d9ffb7f893f4d7ab793088210f58d4ce
monica <monicakrystal@icloud.com>
2025-11-27 13:14:50 -0800

    design and styling for recipe slug and css

--- app/recipes/[slug]/page.tsx
+++ app/recipes/[slug]/page.tsx
@@ -1,9 +1,37 @@
-import { exampleRecipes } from "../../../data/recipes"
+import Image from "next/image";
+import { exampleRecipes } from "../../../data/recipes";
+import styles from "./page.module.css"
 
-const Page = () => {
+type RecipePageProps = {
+  params: Promise<{
+    slug: string;
+  }>;
+};
+
+export default async function Page({ params }: RecipePageProps) {
+
+  const { slug } = await params;
+
+  const recipe = exampleRecipes.find((r) => r.slug === slug);
+
+  if (!recipe) {
+    return <h1>Recipe not found</h1>;
+  }
+
   return (
-    <div>page</div>
-  )
-}
+    <div className={styles.recipeSlugContainer}>
+        <div className={styles.recipeTitle}>
+            <h1>{recipe.title}</h1>
+            <span>⭐️ ⭐️ ⭐️ ⭐️ ⭐️</span>
+            <span>{recipe.labels.join(", ")}</span>
+        </div>
 
-export default Page
+      <Image src={recipe.imageSrc} alt="Recipe Image"
+        width={400}
+        height={300} />
+
+      
+ 
+    </div>
+  );
+}
