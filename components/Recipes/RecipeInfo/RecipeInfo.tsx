2af7ae2ec7da8e0e471780e362f05a3ead6bfd11
monica <monicakrystal@icloud.com>
2025-11-27 16:38:46 -0800

    spacing and styling

--- components/Recipes/RecipeInfo/RecipeInfo.tsx
+++ components/Recipes/RecipeInfo/RecipeInfo.tsx
@@ -29,11 +29,11 @@ const RecipeInfo = ({recipe} : RecipeInfoProps) => {
                 </div>
                 
                 <div className={styles.miniPic}>
-                    <Image src={recipe.imageSrc} alt="Recipe Image" width={200} height={150} />
+                    <Image src={recipe.imageSrc} alt="Recipe Image" width={150} height={150} />
                 </div>
                 
                 <div className={styles.rating}>
-                    <span>⭐️⭐️⭐️⭐️⭐️</span>
+                    <span>⭐️ ⭐️ ⭐️ ⭐️ ⭐️</span>
                 </div>
             </div>
 
@@ -59,16 +59,32 @@ const RecipeInfo = ({recipe} : RecipeInfoProps) => {
 
         </div>
 
-        <div className={styles.servings}>
+        <div className={styles.fastText}>
         <h3>Servings</h3>
+        <h3>Course</h3>
+        <h3>Cuisine</h3>
+        <h3>Freezer Friendly</h3>
+        <h3>Storage</h3>
         </div>
 
         <div className={styles.ingredients}>
         <h3>Ingredients</h3>
+
+         <ul>
+            <li> 1lb of ground turkey</li>
+        </ul>
         </div>
 
         <div className={styles.instructions}>
         <h3>Instructions</h3>
+        
+        <ol>
+            <li>1 lb ground turkey</li>
+            <li>Mix seasoning</li>
+            <li>Cook for 20 minutes</li>
+        </ol>
+       
+
         </div>
 
         <div className={styles.video}>
