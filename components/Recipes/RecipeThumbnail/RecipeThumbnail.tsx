de76972366a23271a5e4e289c73ae45f7f3d84a4
monica <monicakrystal@icloud.com>
2025-11-30 23:56:02 -0800

    add hover color when clicking on title

--- components/Recipes/RecipeThumbnail/RecipeThumbnail.tsx
+++ components/Recipes/RecipeThumbnail/RecipeThumbnail.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image"
+ import Image from "next/image"
 import styles from "./RecipeThumbnail.module.css"
 import RecipeLabel from "../RecipeLabel/RecipeLabel";
 import Link from "next/link";
