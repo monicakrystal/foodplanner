356d44815ad91681667deec0a3a3cd215f55b42d
monica <monicakrystal@icloud.com>
2025-12-03 16:54:17 -0800

    add my recipes to calculate

--- data/recipes.ts
+++ data/recipes.ts
@@ -30,3 +30,44 @@ export const exampleRecipes = [
     nutrition: "JSON"
   },
 ];
+
+
+
+export const myRecipes = [
+  {
+    name: "Oat Shake (1 cup serving)",
+    nutrition: {
+      calories: 475,
+      protein_g: 9.5,
+      carbs_g: 49.5,
+      fat_g: 26.75,
+      fiber_total_g: 6,
+      fiber_soluble_g: 2.6,
+      fiber_insoluble_g: 3.4
+    }
+  },
+  {
+    name: "Lamb & Broccoli Skillet (1 serving)",
+    nutrition: {
+      calories: 451,
+      protein_g: 26,
+      carbs_g: 9.7,
+      fat_g: 33.7,
+      fiber_total_g: 2.4,
+      fiber_soluble_g: 1.06,
+      fiber_insoluble_g: 1.33
+    }
+  },
+  {
+    name: "Gluten-Free Chicken Breast (1 serving)",
+    nutrition: {
+      calories: 459,
+      protein_g: 74,
+      carbs_g: 3,
+      fat_g: 15,
+      fiber_total_g: 1.7,
+      fiber_soluble_g: 0.5,
+      fiber_insoluble_g: 1.2
+    }
+  }
+];
