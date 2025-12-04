885fab6f80d09885fb6a92ed6ac9dbb59b05ab16
monica <monicakrystal@icloud.com>
2025-12-03 22:31:43 -0800

    add docs to calendar

--- components/Calendar/Calendar.tsx
+++ components/Calendar/Calendar.tsx
@@ -1,4 +1,30 @@
 
+
+/* 
+
+To do:
+    - should I make my own from scratch? or use a component library?
+    - design in figma first
+    - needs day, month, and week view
+    - add to grocery list button?
+    - add recipes to faves, or saves?
+    - 
+    - calculations show ere
+
+Goal:
+    - Allow the user to drag and drop recipes in to plan
+    - saves recipes from past and future
+    - automatic saves so if something goes wrong its still there
+    - copy and paste to other days
+    - Automatically calculate and display total nutrition (calories, protein, carbs, fat, fiber).
+
+Current Scope:
+    - default
+
+Future Improvements: 
+
+*/
+
 const Calendar = () => {
   return (
     <div>Calendar</div>
